64bdf80e22bd40245abd5b202568dfdef448dfac
elzup <guild0105@gmail.com>
2016-10-11 11:14:57 +0900

    Add debug login user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(:email => '99xx999@ms.dendai.ac.jp', :password => 'toshino')
+user.skip_confirmation!
+user.save!
+puts 'Create 1 User'
