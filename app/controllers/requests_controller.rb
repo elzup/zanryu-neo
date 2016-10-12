b6f974b238f4d6ff0ec71fa0f253b194b36f326b
elzup <guild0105@gmail.com>
2016-10-12 11:28:29 +0900

    Delete all button

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,15 +1,14 @@
 class RequestsController < ApplicationController
-  before_action :set_room, only: [:destroy]
+  before_action :set_request, only: [:destroy]
+  before_action :set_relations, only: [:create, :create_all, :destroy_all]
+  before_action :set_month_date, only: [:create_all, :destroy_all]
 
   def create
-    user = User.find(params[:user_id])
-    admin = User.find(params[:admin_id])
-    room = Room.find(params[:room_id])
     request = Request.create!(
         date: params[:date_str],
-        user_id: user.id,
-        admin_id: admin.id,
-        room_id: room.id,
+        user_id: @user.id,
+        admin_id: @admin.id,
+        room_id: @room.id,
     )
     render json: request
   rescue ActiveRecord::RecordNotUnique
@@ -22,20 +21,14 @@ class RequestsController < ApplicationController
   end
 
   def create_all
-    user = User.find(params[:user_id])
-    admin = User.find(params[:admin_id])
-    room = Room.find(params[:room_id])
-    year = params[:year].to_i
-    month = params[:month].to_i
-    t = Date.new(year, month, 1)
-    register_dates = Request.select(:date).month(t).map(&:date)
+    register_dates = Request.select(:date).month(@d).map(&:date)
     requests = []
-    ((t..t.at_end_of_month).step(1.day){}.to_a - register_dates).each do |d|
+    ((@d..@d.at_end_of_month).step(1.day){}.to_a - register_dates).each do |d|
       requests << Request.new(
           date: d.strftime('%Y-%m-%d'),
-          user_id: user.id,
-          admin_id: admin.id,
-          room_id: room.id,
+          user_id: @user.id,
+          admin_id: @admin.id,
+          room_id: @room.id,
       )
     end
     Request.import(requests)
@@ -43,13 +36,25 @@ class RequestsController < ApplicationController
   end
 
   def destroy_all
-
+    render json: Request.month(@d).destroy_all
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_room
+  def set_request
     @request = Request.find(params[:id])
+  end
+
+  def set_relations
+    @user = User.find(params[:user_id])
+    @admin = User.find(params[:admin_id])
+    @room = Room.find(params[:room_id])
+  end
+
+  def set_month_date
+    year = params[:year].to_i
+    month = params[:month].to_i
+    @d = Date.new(year, month, 1)
   end
 
 end
