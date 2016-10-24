56815c624257943d92807e9fe5f2efcd77279240
elzup <guild0105@gmail.com>
2016-10-24 17:55:35 +0900

    Create CSV filename

--- app/controllers/csvs_controller.rb
+++ app/controllers/csvs_controller.rb
@@ -7,8 +7,20 @@ class CsvsController < ApplicationController
     end
     csv = Request.to_csv(requests)
     requests.update_all(exported: true)
+    # username_year_month_timestamp.csv
     respond_to do |format|
-      format.csv { send_data csv }
+      format.csv {
+        send_data(csv, filename: filename)
+      }
     end
+  end
+
+  private
+  def filename
+    return @filename if @filename
+    day = params[:next] ? Date.today.next_month : Date.today
+    month_str = day.strftime("%Y%m")
+    timestamp_str = Time.now.to_i.to_s
+    @filename = "#{current_user.label}_#{month_str}_#{timestamp_str}.csv"
   end
 end
