ec96d1bf48acc63d0a7593a5d75b13302f19cbb5
elzup <guild0105@gmail.com>
2016-10-28 22:09:22 +0900

    Update export buttons ui

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,6 +5,10 @@ class TopController < ApplicationController
     @rooms = Room.all
     if current_user.admin?
       @requests = current_user.admin_requests.live.includes([:room, :user])
+      @current_count = current_user.admin_requests.current_month.fresh.count
+      @next_count = current_user.admin_requests.next_month.fresh.count
+      @current_exported_count = current_user.admin_requests.current_month.not_fresh.count
+      @next_exported_count = current_user.admin_requests.next_month.not_fresh.count
       render :admin
     end
     @requests = current_user.requests.live.includes([:room, :admin])
